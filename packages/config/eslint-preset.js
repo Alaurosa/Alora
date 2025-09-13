775bb58003608f6ac6784798aa7f88cf864811de
Ashley Kim <ashleykim026@gmail.com>
2025-09-13 14:17:26 -0700

    small edits

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,28 +1,28 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint'],
-    rules: {
-        'no-console': 'warn',
-        'no-unused-vars': 'warn',
-        '@typescript-eslint/no-explicit-any': 'off',
-        'prefer-const': 'error',
-        'eqeqeq': 'error',
-    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prefer-const': 'error',
+    'eqeqeq': 'error',
+  },
 };
